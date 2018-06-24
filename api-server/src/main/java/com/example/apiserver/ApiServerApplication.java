8bd80e1b9d46f1e3a6a57926e5d651805e7dd46c
user <jaehoon92@gmail.com>
2018-06-24 23:28:59 +0900

    Added HelloWorld Controller

--- api-server/src/main/java/com/example/apiserver/ApiServerApplication.java
+++ api-server/src/main/java/com/example/apiserver/ApiServerApplication.java
@@ -2,9 +2,19 @@ package com.example.apiserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan
 @SpringBootApplication
-public class ApiServerApplication {
+public class ApiServerApplication extends SpringBootServletInitializer {
+	
+	  //Make sure that everything for .war deployment is there
+	  @Override
+	  protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+	    return application.sources(ApiServerApplication.class);
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiServerApplication.class, args);
